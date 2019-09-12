9137147db91ac61b0af768a1b4a37761412fbb70
unknown <ikbal.toriq@appschef.com>
2019-09-11 22:41:25 -0700

    library

---
+++ app/src/main/java/com/example/testlibrary/Main2Activity.java
@@ -0,0 +1,17 @@
+package com.example.testlibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.toasterlibrary.ToasterMessage;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+    }
+}
